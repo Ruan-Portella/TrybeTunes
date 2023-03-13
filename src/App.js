13f7110215d5f2fdc3556ec9d2fbde2c9210513f
Ruan-Portella <ruanmorales29@gmail.br>
2023-03-13 19:27:36 -0300

    Feat: Resolvendo Problemas de Lint do App

--- src/App.js
+++ src/App.js
@@ -12,13 +12,13 @@ class App extends React.Component {
   render() {
     return (
       <Switch>
-        <Route exact path='/' component={ Login } />
-        <Route exact path='/search' component={ Search } />
-        <Route exact path='/album/:id' component={ Album } />
-        <Route exact path='/favorites' component={ Favorites } />
-        <Route exact path='/profile' component={ Profile } />
-        <Route exact path='/profile/edit' component={ ProfileEdit} />
-        <Route exact path='*' component={ NotFound} />
+        <Route exact path="/" component={ Login } />
+        <Route exact path="/search" component={ Search } />
+        <Route exact path="/album/:id" component={ Album } />
+        <Route exact path="/favorites" component={ Favorites } />
+        <Route exact path="/profile" component={ Profile } />
+        <Route exact path="/profile/edit" component={ ProfileEdit } />
+        <Route exact path="*" component={ NotFound } />
 
       </Switch>
     );
