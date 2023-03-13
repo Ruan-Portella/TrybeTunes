702914630dd0130636397e5de811cd8c2e9b9715
Ruan-Portella <ruanmorales29@gmail.br>
2023-03-13 17:41:53 -0300

    Feat: Resolvendo Exercicio 04

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 import Loading from './Loading';
 // import Loading from './Loading';
@@ -24,11 +25,16 @@ class Header extends React.Component {
     const { names, isLoading } = this.state;
     return (
       <header data-testid="header-component">
-        <p data-testid="header-user-name">
+        <nav>
+          <Link to="/search" data-testid="link-to-search">Search</Link>
+          <Link to="/favorites" data-testid="link-to-favorites">Favorites</Link>
+          <Link to="/profile" data-testid="link-to-profile">Profile</Link>
+        </nav>
+        <div data-testid="header-user-name">
           {
             isLoading ? <Loading /> : names
           }
-        </p>
+        </div>
       </header>
     );
   }
