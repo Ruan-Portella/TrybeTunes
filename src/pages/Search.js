2733a0de8fed7c99f046d450a4affc25ce8b0ab2
Ruan-Portella <ruanmorales29@gmail.br>
2023-03-13 17:53:20 -0300

    Feat: Resolvendo Exercicio 05

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,11 +2,48 @@ import React from 'react';
 import Header from '../components/Header';
 
 class Search extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      InputName: '',
+      DisableButton: true,
+    };
+  }
+
+  handleChange = ({ target }) => {
+    const min = 2;
+    this.setState({ [target.name]: target.value });
+    if (target.value.length >= min) {
+      this.setState({ DisableButton: false });
+    } else {
+      this.setState({ DisableButton: true });
+    }
+  };
+
   render() {
+    const { InputName, DisableButton } = this.state;
     return (
       <section>
         <Header />
         <p data-testid="page-search" />
+        <label>
+          <input
+            data-testid="search-artist-input"
+            type="text"
+            name="InputName"
+            value={ InputName }
+            onChange={ this.handleChange }
+          />
+          <button
+            type="button"
+            data-testid="search-artist-button"
+            disabled={ DisableButton }
+          >
+            Pesquisar
+
+          </button>
+        </label>
       </section>
     );
   }
